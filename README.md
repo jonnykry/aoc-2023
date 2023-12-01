688f0aae0e854181ee870f5e005297412defbfed
jonny <jonnykry93@gmail.com>
2023-12-01 12:06:19 -0800

    day one solution

---
+++ README.md
@@ -0,0 +1,7 @@
+My solutions to [Advent of Code 2023](https://adventofcode.com/2023).
+
+Run solutions with Node, for example, to run Day 1's solution:
+
+```
+    node solutions/day1.js
+```
---
+++ solutions/day1.js
@@ -0,0 +1,72 @@
+import { readFileSync } from 'fs';
+
+// place input in day1input.txt in root dir
+const data = readFileSync('./day1input.txt', 'utf-8');
+const lines = data.split(/\r?\n/);
+
+// Part 1
+
+// sum first digit and last digit of each calibration value
+const sum = (data) => {
+    return data.reduce((acc, curr) => {
+        const digits = curr.match(/[0-9]/g);
+        let num = 0;
+
+        if (digits && digits.length > 1) {
+            const first = digits[0];
+            const last = digits[digits.length - 1];
+            num = Number(`${first}${last}`);
+        } else if (digits && digits.length == 1) {
+            // handle single numbers found for duplicates (i.e., 55)
+            const first = digits[0];
+            num = Number(`${first}${first}`);
+        }
+
+        return acc + num;
+    }, 0);
+};
+
+let answer = sum(lines, 'one');
+console.log(`The answer to Part One is ${answer}`);
+
+// Part 2
+
+const wvPair = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+};
+
+/**
+ * Takes a line and returns a new line containing only digits
+ */
+const parseLine = (line) => {
+    let newLine = '';
+
+    for (let i = 0; i < line.length; i++) {
+        // if it's a number, we take those!
+        if (!isNaN(Number(line[i]))) {
+            newLine += Number(line[i]);
+        } else {
+            // otherwise, try and find a word from the current location in the string
+            // if none is found, that's fine, we don't need it for the resulting string either way
+            for (let key of Object.keys(wvPair)) {
+                if (line.substring(i, i + key.length) === key) {
+                    newLine += wvPair[key];
+                }
+            }
+        }
+    }
+
+    return newLine;
+};
+
+const updatedLines = lines.map((line) => parseLine(line));
+answer = sum(updatedLines, 'two');
+console.log(`The answer to Part Two is ${answer}`);
