80aab1882baf786188ffea0f96b2f2d7601dc1e8
jonny <jonnykry93@gmail.com>
2023-12-02 18:27:51 -0800

    clean-up

--- solutions/day2.ts
+++ solutions/day2.ts
@@ -7,15 +7,29 @@ const data = readFileSync('./inputs/day2input.txt', 'utf-8');
 const RED_COUNT = 12;
 const GREEN_COUNT = 13;
 const BLUE_COUNT = 14;
+const COLORS = {
+    RED: 'red',
+    BLUE: 'blue',
+    GREEN: 'green',
+};
+const INITIAL_COLOR_MAP = {
+    [COLORS.RED]: RED_COUNT,
+    [COLORS.BLUE]: BLUE_COUNT,
+    [COLORS.GREEN]: GREEN_COUNT,
+};
+
+// Initialize colormap
+let colorMap = INITIAL_COLOR_MAP;
 
 // Part 1
 
+let validGameIdSum = 0;
+let productSum = 0;
+
 const lines = data.split(/\r?\n/);
-
-let partOneValidGameIdSum = 0;
-let partTwoMaximumPowerSum = 0;
-
 for (let line of lines) {
+    colorMap = INITIAL_COLOR_MAP;
+
     // Split on 'Game #' and the game sequence
     const parts = line.split(': ');
     const gameId = parts[0];
@@ -23,56 +37,48 @@ for (let line of lines) {
     const game = parts[1];
 
     // Split on ';' and for each round of the game sequence
-    const gameSplits = game.split('; ');
-    const partTwoColorMaximums = {
+    const splitGame = game.split('; ');
+    const colorMaximums = {
         blue: 0,
         red: 0,
         green: 0,
     };
 
     let isValid = true;
-    for (let sequence of gameSplits) {
-        let colorMap = {
-            red: RED_COUNT,
-            blue: BLUE_COUNT,
-            green: GREEN_COUNT,
-        };
-
+    for (let sequence of splitGame) {
         // Split on each move ',' within a game sequence split
         let moves = sequence.split(', ');
+        const tempColorMap = { ...colorMap };
 
         for (let move of moves) {
-            let [quantity, color] = move.split(' ');
-            colorMap[color] = colorMap[color] - Number(quantity);
-            partTwoColorMaximums[color] = Math.max(
-                partTwoColorMaximums[color],
-                Number(quantity)
-            );
-        }
+            const [quantityStr, color] = move.split(' ');
+            const quantity = Number(quantityStr);
 
-        Object.keys(colorMap).map((key) => {
-            if (colorMap[key] < 0) {
+            tempColorMap[color] -= quantity;
+            colorMaximums[color] = Math.max(colorMaximums[color], quantity);
+
+            if (tempColorMap[color] < 0) {
                 isValid = false;
             }
-        });
+        }
     }
 
     // Multiply each color maximum together and sum for part 2
-    partTwoMaximumPowerSum += Object.keys(partTwoColorMaximums).reduce(
-        (acc, curr) => acc * partTwoColorMaximums[curr],
+    productSum += Object.keys(colorMaximums).reduce(
+        (product, color) => product * colorMaximums[color],
         1
     );
 
     if (isValid) {
-        partOneValidGameIdSum += id;
+        validGameIdSum += id;
     }
 }
 
-console.log(`Part one answer: ${partOneValidGameIdSum}`);
+console.log(`Part one answer: ${validGameIdSum}`);
 
 // Part 2
 // In part 2, we're looking to find the maximum of each color
 // to satisfy a game sequence based on what we've seen.
 // We can keep track of this in-line in the Part 1 solution.
 
-console.log(`Part one answer: ${partTwoMaximumPowerSum}`);
+console.log(`Part one answer: ${productSum}`);
