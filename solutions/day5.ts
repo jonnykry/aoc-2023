53736825302c3760bf01104459411125910f4e52
jonny <jonnykry93@gmail.com>
2023-12-05 12:55:32 -0800

    day 5 part 1

--- solutions/day5.ts
+++ solutions/day5.ts
@@ -1,9 +1,82 @@
 import { parseFileLines } from './utils';
 
-const fetchInput = async () => {
-    return await parseFileLines('day5input.txt');
+class Range {
+    destinationStart: number;
+    destinationEnd: number;
+    sourceStart: number;
+    sourceEnd: number;
+    length: number;
+
+    constructor(destination: number, source: number, length: number) {
+        this.destinationStart = destination;
+        this.destinationEnd = destination + length - 1;
+        this.sourceStart = source;
+        this.sourceEnd = source + length - 1;
+        this.length = length;
+    }
+
+    findDestination(value: number): number | null {
+        if (value >= this.sourceStart && value <= this.sourceEnd) {
+            return this.destinationStart + (value - this.sourceStart);
+        }
+
+        return null;
+    }
+}
+
+const mapSeedsToCategory = (ranges: Range[], seeds: number[]): number[] => {
+    let result = [];
+
+    for (let seed of seeds) {
+        result.push(
+            ranges.reduce((mappedSeed, range) => {
+                const newDest = range.findDestination(seed);
+                return newDest !== null ? newDest : mappedSeed;
+            }, seed)
+        );
+    }
+
+    return result;
 };
 
-const lines = await fetchInput();
+const processSeeds = (lines: string[]): number[] => {
+    const seeds = lines[0]
+        .split(': ')[1]
+        .split(' ')
+        .map((num) => parseInt(num));
 
-console.log(lines);
+    // remove first and second lines after processing
+    lines.shift();
+    lines.shift();
+
+    return seeds;
+};
+
+export const partOne = (lines: string[]): number => {
+    let seeds = processSeeds(lines);
+
+    let ranges: Range[] = [];
+    for (let line of lines) {
+        line = line.trim();
+
+        // beginning of new chunk
+        if (line === '') {
+            seeds = [...mapSeedsToCategory(ranges, seeds)];
+            ranges = [];
+        } else if (!line.includes('map')) {
+            let [dest, src, size] = line
+                .trim()
+                .split(' ')
+                .map((num) => parseInt(num));
+
+            ranges.push(new Range(dest, src, size));
+        }
+    }
+
+    return Math.min(...seeds);
+};
+
+const lines = await parseFileLines('day5input.txt');
+const solution = partOne(lines);
+
+console.log(`Part One solution: ${solution}`);
