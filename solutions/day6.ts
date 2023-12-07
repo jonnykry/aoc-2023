0b08f46dd386b2bc88ddbbb4ad65df3b5fe6c04e
jonny <jonnykry93@gmail.com>
2023-12-07 12:13:49 -0800

    be less lazy, parse the file

--- solutions/day6.ts
+++ solutions/day6.ts
@@ -1,5 +1,8 @@
+import { parseFile } from './utils';
+
 const solution = (time: number[], distance: number[]): number => {
     let counts = [0, 0, 0, 0];
+
     for (let i = 0; i < time.length; i++) {
         for (let speed = 0; speed < time[i]; speed++) {
             const d = speed * (time[i] - speed);
@@ -13,9 +16,22 @@ const solution = (time: number[], distance: number[]): number => {
     );
 };
 
-// skipping file parsing since it's straightforward
-const partOne = solution([47, 84, 74, 67], [207, 1394, 1209, 1014]);
-const partTwo = solution([47847467], [207139412091014]);
+const data = await parseFile('day6input.txt');
+const lines = data.split(/\n/);
+const times = lines[0]
+    .split('Time: ')[1]
+    .split(' ')
+    .map((str) => Number(str));
+const distances = lines[1]
+    .split('Distance: ')[1]
+    .split(' ')
+    .map((str) => Number(str));
 
-console.log('Part One solution: ', partOne);
-console.log('Part Two solution: ', partTwo);
+const partOne = solution(times, distances);
+
+const p2Time = Number(times.map((time) => `${time}`).join(''));
+const p2Dist = Number(distances.map((dist) => `${dist}`).join(''));
+const partTwo = solution([p2Time], [p2Dist]);
+
+console.log('Part One solution:', partOne);
+console.log('Part Two solution:', partTwo);
