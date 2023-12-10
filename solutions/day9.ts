b3693e1ebf8553bd9b57b37b8faa0cf36d452a7a
jonny <jonnykry93@gmail.com>
2023-12-09 19:13:00 -0800

    consolidate solution code flip

--- solutions/day9.ts
+++ solutions/day9.ts
@@ -1,24 +1,29 @@
 import { parseFile } from './utils';
 
-const solve = (lines: string[]) => {};
-
-export const partOne = (data: string): number => {
-    const lines = data.split(/\n/);
+const solve = (lines: string[], isPartTwo: boolean = false) => {
     let results: number[] = [];
 
     lines.map((line: string) => {
         let values = line.split(' ').map((val) => parseInt(val));
 
-        let rightSideValues = [];
-        rightSideValues.unshift(values[values.length - 1]);
+        let sideValues = [];
+        sideValues.push(values[isPartTwo ? 0 : values.length - 1]);
         while (true) {
             let sequence = [];
 
-            for (let i = 1; i < values.length; i++) {
-                sequence.push(values[i] - values[i - 1]);
+            if (isPartTwo) {
+                for (let i = values.length - 2; i >= 0; i--) {
+                    sequence.unshift(values[i + 1] - values[i]);
+                }
+            } else {
+                for (let i = 1; i < values.length; i++) {
+                    sequence.push(values[i] - values[i - 1]);
+                }
             }
 
-            rightSideValues.unshift(sequence[sequence.length - 1]);
+            sideValues.unshift(
+                isPartTwo ? sequence[0] : sequence[sequence.length - 1]
+            );
 
             if (sequence.filter((val) => val !== 0).length === 0) {
                 break;
@@ -28,9 +33,11 @@ export const partOne = (data: string): number => {
         }
 
         let newSequence = [0];
-        for (let i = 1; i < rightSideValues.length; i++) {
+        for (let i = 1; i < sideValues.length; i++) {
             newSequence.push(
-                rightSideValues[i] + newSequence[newSequence.length - 1]
+                isPartTwo
+                    ? sideValues[i] - newSequence[newSequence.length - 1]
+                    : sideValues[i] + newSequence[newSequence.length - 1]
             );
         }
 
@@ -40,42 +47,14 @@ export const partOne = (data: string): number => {
     return results.reduce((sum, curr) => sum + curr, 0);
 };
 
+export const partOne = (data: string): number => {
+    const lines = data.split(/\n/);
+    return solve(lines, false);
+};
+
 export const partTwo = (data: string): number => {
     let lines = data.split(/\n/);
-    let results: number[] = [];
-
-    lines.map((line: string) => {
-        let values = line.split(' ').map((val) => parseInt(val));
-
-        let leftSideValues = [];
-        leftSideValues.push(values[0]);
-        while (true) {
-            let sequence = [];
-
-            for (let i = values.length - 2; i >= 0; i--) {
-                sequence.unshift(values[i + 1] - values[i]);
-            }
-
-            leftSideValues.unshift(sequence[0]);
-
-            if (sequence.filter((val) => val !== 0).length === 0) {
-                break;
-            } else {
-                values = sequence;
-            }
-        }
-
-        let newSequence = [0];
-        for (let i = 1; i < leftSideValues.length; i++) {
-            newSequence.push(
-                leftSideValues[i] - newSequence[newSequence.length - 1]
-            );
-        }
-
-        results.push(newSequence[newSequence.length - 1]);
-    });
-
-    return results.reduce((sum, curr) => sum + curr, 0);
+    return solve(lines, true);
 };
 
 const data = await parseFile('day9input.txt');
