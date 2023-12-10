5ece08e99ebf2bada453587c02450024a2a6c8f9
jonny <jonnykry93@gmail.com>
2023-12-09 19:32:35 -0800

    cleaner approach for reversing

--- solutions/day9.ts
+++ solutions/day9.ts
@@ -4,26 +4,24 @@ const solve = (lines: string[], isPartTwo: boolean = false) => {
     let results: number[] = [];
 
     lines.map((line: string) => {
-        let values = line.split(' ').map((val) => parseInt(val));
+        let values: number[] = line.split(' ').map((val) => parseInt(val));
+        if (isPartTwo) {
+            values = values.reduce((reversed: number[], curr: number) => {
+                reversed.unshift(curr);
+                return reversed;
+            }, []);
+        }
 
         let sideValues = [];
-        sideValues.push(values[isPartTwo ? 0 : values.length - 1]);
+        sideValues.push(values[values.length - 1]);
         while (true) {
             let sequence = [];
 
-            if (isPartTwo) {
-                for (let i = values.length - 2; i >= 0; i--) {
-                    sequence.unshift(values[i + 1] - values[i]);
-                }
-            } else {
-                for (let i = 1; i < values.length; i++) {
-                    sequence.push(values[i] - values[i - 1]);
-                }
+            for (let i = 1; i < values.length; i++) {
+                sequence.push(values[i] - values[i - 1]);
             }
 
-            sideValues.unshift(
-                isPartTwo ? sequence[0] : sequence[sequence.length - 1]
-            );
+            sideValues.unshift(sequence[sequence.length - 1]);
 
             if (sequence.filter((val) => val !== 0).length === 0) {
                 break;
@@ -35,9 +33,7 @@ const solve = (lines: string[], isPartTwo: boolean = false) => {
         let newSequence = [0];
         for (let i = 1; i < sideValues.length; i++) {
             newSequence.push(
-                isPartTwo
-                    ? sideValues[i] - newSequence[newSequence.length - 1]
-                    : sideValues[i] + newSequence[newSequence.length - 1]
+                sideValues[i] + newSequence[newSequence.length - 1]
             );
         }
 
